cd73e23db82c6931d6d8ece78c04ad74637797d9
ilgar31 <ilgar.bagishev@gmail.com>
2024-02-05 10:33:59 +0300

    added main page and one table

--- project_site/main/models.py
+++ project_site/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Reviews(models.Model):
+    name = models.CharField("ФИО", max_length=45, blank=True)
+    review = models.CharField("Отзыв", max_length=45, blank=True)
+    date = models.DateTimeField("Дата и время отзыва", default=datetime.now, blank=True)
+
+
+    def __str__(self):
+        return f'{self.name} {self.date}'
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
